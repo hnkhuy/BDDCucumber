04f73411b8eb771b13e957cd25ebdbb049cf6e86
HuyHuynh <huy.huynh@titancorpvn.com>
2018-08-22 21:29:39 +0700

    Add cucumber and testng libs

---
+++ src/main/java/Page/LoginPage.java
@@ -0,0 +1,7 @@
+package Page;
+
+/**
+ * Created by huy.huynh on 22/08/2018.
+ */
+public class LoginPage {
+}
---
+++ src/main/java/StepDefs/DemoStepDef.java
@@ -0,0 +1,7 @@
+package StepDefs;
+
+/**
+ * Created by huy.huynh on 22/08/2018.
+ */
+public class DemoStepDef {
+}
