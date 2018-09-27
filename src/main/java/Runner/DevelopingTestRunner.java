dc47111f959dbcf58d4127bdf8873d32d1ca4bf7
HuyHuynhZ <huy.huynh@titancorpvn.com>
2018-09-27 18:24:17 +0700

    done basic report of Cucumber

--- src/main/java/Runner/DevelopingTestRunner.java
+++ src/main/java/Runner/DevelopingTestRunner.java
@@ -1,27 +1,34 @@
 package Runner;
 
-import Center.WebDriverCenter;
 import Utilities.PropertiesFileReader;
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.io.File;
 
 /**
  * Created by huy.huynh on 13/09/2018.
  */
-
-@CucumberOptions(features = "src/main/resources/features/", glue = {"StepDefs"})
+@CucumberOptions(features = "src/main/resources/features/", glue = {"StepDefs"}, plugin = {"com.cucumber.listener" +
+        ".ExtentCucumberFormatter:target/cucumber-reports/report.html"})
 public class DevelopingTestRunner extends AbstractTestNGCucumberTests {
     @BeforeTest
     public void setupSomething() throws Exception {
-//        WebDriverCenter.setupPrimaryWebDriver();
         PropertiesFileReader.openFileProperty();
+    }
+
+    @AfterClass
+    public static void writeExtentReport() throws Exception {
+        Reporter.loadXMLConfig(new File(PropertiesFileReader.getPropertyOfConfigFile("reportConfigPath")));
     }
 
     @AfterTest
     public void tearDownTest() {
-//        WebDriverCenter.quitAllDriver();
+        //        WebDriverCenter.quitAllDriver();
         PropertiesFileReader.closePropertiesFileInputStream();
     }
 }
